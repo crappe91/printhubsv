f21c68681e722148f47f3e949c182419843c45ca
Christian <crappe1991@gmail.com>
2024-02-05 22:08:21 +0100

    basic structure

---
+++ src/test/java/com/digitalservice/printhub/PrinthubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalservice.printhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrinthubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
