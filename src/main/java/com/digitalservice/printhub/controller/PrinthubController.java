267985f1081db93427b2cb9019e6fb8522b82652
Christian <crappe1991@gmail.com>
2024-02-07 21:56:44 +0100

    estructura basica

---
+++ src/main/java/com/digitalservice/printhub/controller/PrinthubController.java
@@ -0,0 +1,14 @@
+package com.digitalservice.printhub.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PrinthubController {
+//    @PostMapping("/printhubposting")
+//    public String handlePostRequest(@RequestBody String json) {
+//        // Procesar el JSON recibido
+//        return "JSON recibido: " + json;
+//    }
+}
