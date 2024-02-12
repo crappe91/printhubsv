598b79fba03f0d72ca5fcae3443d6e372ea8efcd
Christian <crappe1991@gmail.com>
2024-02-12 23:22:53 +0100

    Generator Configuration
    [Generator] generado controller y clases de ejemplo realizadas, falta arrancar

--- src/main/java/com/digitalservice/printhub/controller/PrinthubController.java
+++ src/main/java/com/digitalservice/printhub/controller/PrinthubController.java
@@ -1,14 +1,26 @@
 package com.digitalservice.printhub.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class PrinthubController {
-//    @PostMapping("/printhubposting")
-//    public String handlePostRequest(@RequestBody String json) {
-//        // Procesar el JSON recibido
-//        return "JSON recibido: " + json;
-//    }
+import com.digitalservice.api.PresupuestoApiController;
+import com.digitalservice.api.PresupuestoApiDelegate;
+import com.digitalservice.model.Presupuesto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.Parameter;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.time.OffsetDateTime;
+
+
+public class PrinthubController extends PresupuestoApiController {
+
+
+    public PrinthubController(PresupuestoApiDelegate delegate) {
+        super(delegate);
+    }
+
+
 }
